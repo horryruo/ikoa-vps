ccf451ca5abe6ad7b4671b2b2933bf578e1ef87f
ruoxin <33121172+horryruo@users.noreply.github.com>
2020-10-22 18:53:19 +0800

    beta

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
-from webapp import loadini
+from loadini import read_config
 
 def start():
-    conf_data = loadini.read_config()
+    conf_data = read_config()
     cmd = 'ts bash start.sh {} {} {} {} {} {}'.format(conf_data['serial_code'],conf_data['team_drive_id'],conf_data['sa_1'],conf_data['sa_1'],conf_data['merge_bool'],conf_data['output_filename'])
     #print(cmd)
     os.system(cmd)
